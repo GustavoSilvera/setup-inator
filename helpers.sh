31ed5f1cedae973b6c971d971c3fe24f264dc300
GustavoSilvera <grs5382@gmail.com>
2021-06-28 23:41:52 -0700

    fixed installation on ubuntu 20.10 test

--- helpers.sh
+++ helpers.sh
@@ -9,10 +9,13 @@ NC='\033[0m'
 set -e
 
 run_install() {
+	# expects arg 1 to be the installer cmd as a string
+	# expects arg 2 to be all the packages to install
 	INSTALLER=$1
-	PKGS=$2
+	shift # shift all arguments to the left
+	PKGS=("$@")
 	echo -e "${CYAN}Starting \"${INSTALLER}\" installation process ${NC}"
-	for i in ${PKGS[*]} ; do
+	for i in ${PKGS[@]} ; do
 	    eval ${INSTALLER} $i || (echo -e "${RED}Failed installing $i ${NC}" && exit 1)
 		echo -e "${GREEN}Successfully installed $i ${NC}"
 	done
@@ -22,9 +25,9 @@ run_git_cloner() {
 	# takes in the repos as a list of ("username"/"reponame", ...)
 	mkdir -p git_repos # new folder
 	cd git_repos
-	GIT_REPOS=$1
+	GIT_REPOS=("$@")
 	# extract the username and reponame
-	for userrepo in ${GIT_REPOS[*]} ; do
+	for userrepo in ${GIT_REPOS[@]} ; do
 		username=$(cut -d'/' -f1 <<<"${userrepo}")
 		reponame=$(cut -d'/' -f2 <<<"${userrepo}")
 		if [ ! -d $reponame ] # only clone once
@@ -49,8 +52,9 @@ install_zsh_plugins() {
 	echo -e "${CYAN}Installing zsh & plugins${NC}"
 	# install zsh
 	if [ ! -d "${HOME}/.oh-my-zsh/" ]
-	then 
-		bash ./git_repos/ohmyzsh/tools/install.sh # only run script once
+	then
+	    echo -e "Running zsh installer"
+	    bash ./git_repos/ohmyzsh/tools/install.sh # only run script once
 	fi
 	# install zsh plugins
 	if [ ! -d "${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]
@@ -60,5 +64,22 @@ install_zsh_plugins() {
 		cp -r ./git_repos/fast-syntax-highlighting ${PLUGINS_DIR}
 		# add plugins to .zshrc under "plugins=(git)"
 		sed -ie "s/plugins=(git)/\plugins=(git)\nplugins=(zsh-autosuggestions fast-syntax-highlighting)/" $HOME/.zshrc
+		echo -e "Installed zsh plugins"
 	fi
+}
+
+download_files() {
+    mkdir -p downloaded_files
+    cd downloaded_files
+    DOWNLOAD_TUPLES=($@)
+    for link_name_tuple in ${DOWNLOAD_TUPLES[@]} ; do
+	LINK=$(cut -d',' -f1 <<<"${link_name_tuple}")
+	NAME=$(cut -d',' -f2 <<<"${link_name_tuple}") 
+	if [ ! -d "./${NAME}.deb" ]
+	then
+	    echo -e "${CYAN}Downloading to \"${NAME}.deb\"${NC}"
+	    wget ${LINK} -O ${NAME}.deb
+	fi
+    done
+    cd ..
 }
--- linux.sh
+++ linux.sh
@@ -14,13 +14,16 @@ APT_PKGS=(
     "git"
     "tree"
     "tmux"
+    "curl"
     "neofetch"
     "htop"
     "emacs-nox"
-	"terminator"
+    "terminator"
+    "zsh"
+    "chromium-browser"
 )
 
-run_install "sudo apt install -y" ${APT_PKGS}
+run_install "sudo apt install -y" ${APT_PKGS[@]}
 
 # install packaged through git
 ZSH_REPOS=( # insert username/reponame
@@ -29,8 +32,20 @@ ZSH_REPOS=( # insert username/reponame
     "zdharma/fast-syntax-highlighting"
 )
 
-run_git_cloner ${ZSH_REPOS}
+run_git_cloner ${ZSH_REPOS[@]}
 
-install_zsh ${HOME}
+install_zsh_plugins ${HOME}
+
+# formatted as (single str) "link,resulting_name"
+DOWNLOAD_LINKS=(
+    "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64,vscode"
+    
+)
+
+download_files ${DOWNLOAD_LINKS[@]}
+
+for deb_file in ./downloaded_files/*.deb ; do
+    sudo apt install ${deb_file}
+done
 
 cd .. # back to main dir
