331cc84eef60df75e68b3b823417d0d93a9669a3
GustavoSilvera <gustavo.r.silvera@gmail.com>
2021-06-30 01:10:21 -0700

    added other plasma/kwin config files & scripts

---
+++ configs/kde-plasma/plasma_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:kubuntu-ppa/backports
+
+sudo apt update
+
+sudo apt install kde-plasma-desktop
+
+# reboot
---
+++ configs/kde-plasma/plasma_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source ../../helpers.sh
+
+# special thanks to this repo: https://github.com/shalva97/kde-configuration-files
+
+# copy to .kde folder
+cp -r ./share ~/.kde/
+
+# copy over plasma appletsrc (plasmoids)
+cp ./plasma-org.kde.plasma.desktop-appletsrc ~/.config/
+
+# copy over workspace/kwin rules
+cp ./kwinrc ~/.config/
+
+# copy over window rules
+cp ./kwinrulesrc ~/.config/
+
+# copy over wallpaper
+mkdir -p ~/Pictures/Wallpapers/
+cp ./icecold2.png ~/Pictures/Wallpapers/
+
+PAPIRUS_REPOS=(
+	"PapirusDevelopmentTeam/papirus-icon-theme"
+)
+cp -r git_repos/Papirus-Dark/ ~/.local/share/icons/
